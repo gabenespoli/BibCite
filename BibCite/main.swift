a5462c6f9d7294aa375e6d2bb13e63f117b0cc00
Gabe Nespoli <gabenespoli@gmail.com>
2016-06-23 02:22:03 -0400

    Initial Commit

---
+++ BibCite/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BibCite
+//
+//  Created by Gabe Nespoli on 2016-06-23.
+//  Copyright © 2016 Gabriel A. Nespoli & Alex Andrews. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
