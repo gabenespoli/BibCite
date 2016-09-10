68f647355927dc86b9b23957b186e323ed326aea
Gabe Nespoli <gabenespoli@gmail.com>
2016-09-10 12:33:00 -0400

    added some string parsing to convert the string of a bib entry to a dict

--- BibCite/Citation.swift
+++ BibCite/Citation.swift
@@ -21,10 +21,35 @@ struct Citation{
      */
     static func make(string: String) -> Citation{
         
-        let citeKey = "Bidelman2009"
-        let authorList = ["Bidelman","Gavin M and Krishnan","Ananthanarayan"]
+        var str = "Bidelman2009,\nauthor = {Bidelman, Gavin M and Krishnan, Ananthanarayan},\nfile = {:Users/gmac/Documents/Mendeley/Bidelman, Krishnan/Bidelman, Krishnan - 2009 - Neural Correlates of Consonance, Dissonance, and the Hierarchy of Musical Pitch in the Human Brainstem.pdf:pdf},\njournal = {Journal of Neuroscience},\nmonth = {oct},\nnumber = {42},\npages = {13165--13171},\ntitle = {{Neural Correlates of Consonance, Dissonance, and the Hierarchy of Musical Pitch in the Human Brainstem}},\nurl = {http://www.jneurosci.org/cgi/doi/10.1523/JNEUROSCI.3900-09.2009 papers3://publication/doi/10.1523/JNEUROSCI.3900-09.2009},\nvolume = {29},\nyear = {2009}"
+
+        // split by newlines
+        let fields = str.characters.split(separator: "\n").map{String($0)}
         
-        return Citation(key: citeKey, authors: authorList)
+        var fieldsDict: [String:String] = [:] // initalize dictionary
+        
+        // add cite key to dictionary
+        fieldsDict["citeKey"] = fields[0].trimmingCharacters(in: .whitespacesAndNewlines)
+        fieldsDict["citeKey"] = fieldsDict["citeKey"]?.trimmingCharacters(in: ["}","{",","])
+        
+        // add rest of info to dictionary
+        // start at 1 because citekey is done above because it doesn't have an "=" to split by
+        for i in 1...fields.count-1 {
+            var fieldsTrimmed = fields[i].trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            var temp = fieldsTrimmed.characters.split(separator: "=").map{String($0)}
+            
+            for j in 0...temp.count-1 {
+                temp[j] = temp[j].trimmingCharacters(in: .whitespacesAndNewlines)
+                temp[j] = temp[j].trimmingCharacters(in: ["}","{",","])
+                temp[j] = temp[j].trimmingCharacters(in: .whitespacesAndNewlines)
+            }
+            
+            fieldsDict["\(temp[0])"] = temp[1]
+        }
+
+        
+        return Citation(key: fieldsDict["citeKey"]!, authors: fieldsDict["author"]!)
     }
     
     /**
