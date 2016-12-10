89e7590d4e034842b649575f690791cdfb4d189b
Alex Andrews <alexjamesa@gmail.com>
2016-12-10 13:31:38 -0500

    Add search functionality

--- BibCiteTests/LibraryTests.swift
+++ BibCiteTests/LibraryTests.swift
@@ -7,26 +7,51 @@
 //
 
 import XCTest
+@testable import BibCite
 
 class LibraryTests: XCTestCase {
-
-    func testThatInitLoadsCountOfExpectedData(){
-        
-    }
     
-    func testThatInitLoadsExpectedDataSamples(){ // Check first and last
-        
-    }
+    let testCitations:[Citation] = [
+        Citation(key: "Gabe2011", authors: ["barry","dogman"]),
+        Citation(key: "Gabe2017", authors: ["barry","fish"]),
+        Citation(key: "Alex2015", authors: ["horse","dogman"]),
+        Citation(key: "Alex1788", authors: ["eddie","patch"])]
+    
     
     func testThatItReturnsNothingForEmptySearch(){
+        // given
         
+        let library = Library(citations: testCitations)
+        
+        // when
+        let matches = library.search(query: "Batman")
+        
+        // then
+        XCTAssertEqual(matches.count, 0)
     }
     
     func testThatItReturnsKnownResultForSingleWordSearch(){
+        // given
+        let library = Library(citations: testCitations)
         
+        // when
+        let matches = library.search(query: "Gabe")
+        
+        // then
+        XCTAssertEqual(matches.count, 2)
+        XCTAssertEqual(matches[0].key, "Gabe2011")
+        XCTAssertEqual(matches[1].key, "Gabe2017")
     }
     
     func testThatItReturnsKnownResultForMultiWordSearch(){
+        // given
+        let library = Library(citations: testCitations)
         
+        // when
+        let matches = library.search(query: "patch")
+        
+        // then
+        XCTAssertEqual(matches.count, 1)
+        XCTAssertEqual(matches.first!.key, "Alex1788")
     }
 }
